ada0a8c53c4ffaf23f8e86a1c83735033dacfbcf
schmids3 <schmids3@bzz.ch>
2025-01-01 22:42:44 +0100

    Improve chat application: add multiple user handling

--- backend/user.py
+++ backend/user.py
@@ -1,7 +1,48 @@
 import socket
+import threading
+import sys
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 3000))
+username = input('Choose a username: ')
 
-user_message = client.send(input("Enter message here: ").encode())
-print(client.recv(1024).decode())
+user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    user.connect(('localhost', 3001))
+except:
+    print("Unable to connect to the server.")
+    sys.exit()
+
+
+def receive():
+    while True:
+        try:
+            message = user.recv(1024).decode()
+            if message == "USERNAME":
+                user.send(username.encode())
+            elif message.startswith("ERROR:"):
+                print(message)
+                user.close()
+                sys.exit()
+            else:
+                print(message)
+        except:
+            print('An error occurred while receiving messages.')
+            user.close()
+            break
+
+
+def texting():
+    while True:
+        try:
+            message = input()
+            user.send(f"{username}: {message}".encode())
+        except:
+            print("An error occurred while sending messages.")
+            user.close()
+            break
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+texting_thread = threading.Thread(target=texting)
+texting_thread.start()
